2e0434465afceba84ec26d9f19a5ac9103205706
Priyanshu Ranjan <priyanshu2004mishra@gmail.com>
2026-08-25 02:07:39 +0530

    feat: initialize Spring Boot backend

---
+++ backend/sentinelcore-backend/src/test/java/com/sentinelcore/sentinelcore_backend/SentinelcoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sentinelcore.sentinelcore_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelcoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
